619fe6297d098706543f292fa72d6c69f2ec90d7
Jesse Jafa <jesse.jafa@gmail.com>
2024-05-26 15:22:58 +0300

    remove unneeded statements

--- examples/trigger_single/src/main.rs
+++ examples/trigger_single/src/main.rs
@@ -1,11 +1,9 @@
 extern crate novu_rust;
 use novu_rust::subscribers::SubscriberPayloadDto;
-use novu_rust::Novu;
-use novu_rust::TriggerPayload;
+use novu_rust::{Novu, TriggerPayload};
 #[async_std::main]
 async fn main() {
     let novu = Novu::new(std::env::var("API_KEY").unwrap(), None).unwrap();
-    let p = novu.subscribers.get("1".to_string()).await;
     let result = novu
         .trigger(TriggerPayload {
             name: "testing".into(),
